06279a93b8dcdc2a6aab99cc0b2b7aa4635d4a40
Guilherme Soares <guilhermesoares1996@hotmail.com>
2022-07-25 01:18:04 -0300

    add UseCors

--- IfrsDocs.API/Startup.cs
+++ IfrsDocs.API/Startup.cs
@@ -52,6 +52,8 @@ namespace IfrsDocs.API
 
             app.UseAuthorization();
 
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
