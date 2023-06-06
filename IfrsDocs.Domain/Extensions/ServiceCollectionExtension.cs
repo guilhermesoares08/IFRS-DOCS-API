2d24063acd105025027a96721ba5fb75629fcd6f
Guilherme Soares <guilhermesoares1996@hotmail.com>
2023-06-06 00:34:40 -0300

    remove baseService addScoped

--- IfrsDocs.Domain/Extensions/ServiceCollectionExtension.cs
+++ IfrsDocs.Domain/Extensions/ServiceCollectionExtension.cs
@@ -9,7 +9,6 @@ namespace IfrsDocs.Domain
     {
         public static IServiceCollection AddServices(this IServiceCollection services)
         {
-            services.AddScoped(typeof(IBaseService<>), typeof(BaseService<,>));            
             services.AddScoped<ICourseService, CourseService>();
             services.AddScoped<IDocumentOptionService, DocumentOptionService>();
             services.AddScoped<IFormService, FormService>();
