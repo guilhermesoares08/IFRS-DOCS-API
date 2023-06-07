825439228837ca4607153cac17037fbb547e0f59
Guilherme Soares <guilhermesoares1996@hotmail.com>
2023-06-06 22:32:28 -0300

    remove isDevelopment from swagger

--- IfrsDocs.API/Startup.cs
+++ IfrsDocs.API/Startup.cs
@@ -92,13 +92,11 @@ namespace IfrsDocs.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IfrsDocs.API v1"));
-                app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-            }
+            
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "IfrsDocs.API v1"));
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());            
 
             app.UseHttpsRedirection();
 
