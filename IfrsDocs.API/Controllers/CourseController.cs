d8c61d19a69e143f7c995ede67b1a1044f1cdf75
Guilherme Soares <guilhermesoares1996@hotmail.com>
2023-06-14 23:07:39 -0300

    adicionado endpoint de cursos

---
+++ IfrsDocs.API/Controllers/CourseController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using IfrsDocs.Domain;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace IfrsDocs.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CourseController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ICourseService _courseService;
+
+        public CourseController(IMapper mapper, ICourseService courseService)
+        {
+            _mapper = mapper;
+            _courseService = courseService;
+        }
+
+        [HttpGet("getAll")]
+        [AllowAnonymous]
+        public IActionResult GetAllCourses()
+        {
+            try
+            {
+                List<Course> results = _courseService.GetAllAsync().Result;
+                List<CourseDto> resultMap = _mapper.Map<List<CourseDto>>(results);
+                return Ok(resultMap);
+            }
+            catch (System.Exception ex)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, $"Erro ao tentar recuperar Cursos {ex.Message}");
+            }
+        }
+    }
+}
